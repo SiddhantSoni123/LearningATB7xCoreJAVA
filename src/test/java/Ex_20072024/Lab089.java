a60720a45d17a6748b1d8b7b22d92d2ac2ec18fb
Siddhant Soni <siddhantsoni846@gmail.com>
2024-07-23 21:32:59 +0530

    20th July, 2024 - 2nd Commit

---
+++ src/test/java/Ex_20072024/Lab089.java
@@ -0,0 +1,13 @@
+package Ex_20072024;
+
+import java.util.Scanner;
+
+public class Lab089 {
+    public static void main(String[] args) {
+
+        //int yrs;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your age");
+        //yrs=sc.nextInt();
+    }
+}
