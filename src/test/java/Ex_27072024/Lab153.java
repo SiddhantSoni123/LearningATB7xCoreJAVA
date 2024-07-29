9d36dff5463eaa4352428303fb0df498c160918b
Siddhant Soni <siddhantsoni846@gmail.com>
2024-07-29 13:44:59 +0530

    27th July, 2024 - 1st Commit

---
+++ src/test/java/Ex_27072024/Lab153.java
@@ -0,0 +1,8 @@
+package Ex_27072024;
+
+public class Lab153 {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder("Siddhant");
+        System.out.println(stringBuilder.reverse().toString());
+    }
+}
