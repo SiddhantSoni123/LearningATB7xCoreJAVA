a6fff9381ab053a96388d04951a05871ba69a0cd
Siddhant Soni <siddhantsoni846@gmail.com>
2024-07-30 14:42:58 +0530

    27th July, 2024 - 2nd Commit

--- src/test/java/Ex_27072024/Lab147.java
+++ src/test/java/Ex_27072024/Lab147.java
@@ -7,7 +7,7 @@ public class Lab147 {
         // Find the Max in the Arrays
         // Find the Max Salary, Min  in the Salary Array
         int[] salaries = {7, 10, 4, 13, 25, 18, 9, 8, 17};
-        // Infsys - Autoamtion Interview
+        // Infosys - Automation Interview Ques
         int max = salaries[0];
         int min = salaries[0];
         for (int i = 0; i < salaries.length ; i++) {
