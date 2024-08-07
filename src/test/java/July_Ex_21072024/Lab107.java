8a561968abd39fa5479ca70f9a05fa6bb28e2575
Siddhant Soni <siddhantsoni846@gmail.com>
2024-08-07 11:51:01 +0530

    21st July, 2024 - 2nd Commit

---
+++ src/test/java/July_Ex_21072024/Lab107.java
@@ -0,0 +1,15 @@
+package July_Ex_21072024;
+
+public class Lab107 {
+    public static void main(String[] args) {
+
+        int age  = 34;
+        int pramod_dutta = 34;  //Although the datatype is int. The variable_name can be anything 
+                                //be it pramod_dutta or siddhant_soni - Important point
+        String name = "Amit";
+
+        for (int pramod=1; pramod < 6; pramod++) { // 1 to 5
+            System.out.println(pramod); /// 5<5
+        }
+    }
+}
