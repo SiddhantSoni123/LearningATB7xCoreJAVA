aa03748e1ebf29ba6e0a94b13b2dff6ef90f26f3
Siddhant Soni <siddhantsoni846@gmail.com>
2024-08-07 11:43:53 +0530

    14th July, 2024 - 2nd Commit

--- src/test/java/Ex_14072024/Lab077.java
+++
@@ -1,8 +0,0 @@
-package Ex_14072024;
-
-public class Lab077 {
-    public static void main(String[] args) {
-        String s1 = "pramod";
-        // Non Primitive Types - String, Arrays, Class, ENUM.. more will come
-    }
-}
