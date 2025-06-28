e116400a07943a8535f101024fe61092427c913c
Siddhant Soni <siddhantsoni846@gmail.com>
2025-06-28 06:53:37 +0530

    18th August, 2024 - 1st Commit

---
+++ src/test/java/Aug_Ex_18082024/Collection_Framework/Lab230.java
@@ -0,0 +1,13 @@
+package Aug_Ex_18082024.Collection_Framework;
+
+import java.util.List;
+
+    public class Lab230 {
+        public static void main(String[] args) {
+            List shopping_list = List.of("Milk","Bread","Butter","Cheese");
+            System.out.println(shopping_list);
+            System.out.println(shopping_list.size());
+            shopping_list.add("Banana");
+
+        }
+}
---
+++ src/test/java/Aug_Ex_18082024/Collection_Framework/Lab233.java
@@ -0,0 +1,16 @@
+package Aug_Ex_18082024.Collection_Framework;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+    public class Lab233 {
+        public static void main(String[] args) {
+            List<Integer> marks = new ArrayList();
+            marks.add(100);
+//        marks.add("pramod");
+//        marks.add(true);
+//        Collection -I
+//        Collections -C
+        }
+}
